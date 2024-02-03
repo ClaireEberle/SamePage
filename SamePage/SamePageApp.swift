2b88bd868c21d152d6925126c0df26aa39a4bc66
Claire <claire.3.eberle@gmail.com>
2024-02-03 10:52:29 -0800

    Initial Commit

---
+++ SamePage/SamePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamePageApp.swift
+//  SamePage
+//
+//  Created by Claire Eberle on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SamePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
